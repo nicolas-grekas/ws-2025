60f47286d599a5ae58ee1c377cca5a3a4790ec8b
Nicolas Grekas <nicolas.grekas@gmail.com>
2025-06-10 15:05:12 +0200

    feedback form

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,11 +2,16 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Conference;
+use App\Form\CommentTypeForm;
 use App\Repository\CommentRepository;
 use App\Repository\ConferenceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\DependencyInjection\Attribute\AutowireMethodOf;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Attribute\MapQueryParameter;
 use Symfony\Component\Routing\Attribute\Route;
@@ -23,7 +28,11 @@ final class ConferenceController extends AbstractController
 
     #[Route('/conference/{slug:conference}', name: 'conference')]
     public function show(
+        Request $request,
         Conference $conference,
+        EntityManagerInterface $entityManager,
+        #[Autowire(param: 'photo_dir')]
+        string $photoDir,
 
         #[AutowireMethodOf(CommentRepository::class)]
         \Closure $getCommentPaginator,
@@ -33,6 +42,26 @@ final class ConferenceController extends AbstractController
 
     ): Response
     {
+        $comment = new Comment();
+        $form = $this->createForm(CommentTypeForm::class, $comment);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setConference($conference);
+
+            if ($photo = $form['photo']->getData()) {
+                $filename = bin2hex(random_bytes(6)).'.'.$photo->guessExtension();
+                $photo->move($photoDir, $filename);
+                $comment->setPhotoFilename($filename);
+            }
+
+            $entityManager->persist($comment);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+        }
+
         $paginator = $getCommentPaginator($conference, $offset);
 
         return $this->render('conference/show.html.twig', [
@@ -40,6 +69,7 @@ final class ConferenceController extends AbstractController
             'comments' => $paginator,
             'previous' => $offset - CommentRepository::COMMENTS_PER_PAGE,
             'next' => min(count($paginator), $offset + CommentRepository::COMMENTS_PER_PAGE),
+            'comment_form' => $form,
         ]);
     }
 }
